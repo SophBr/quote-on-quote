41db1a6a3dd24cde0f6257f6812d3a8aef3fe565
SophBr <sophie.breunig@yahoo.de>
2022-02-12 16:38:04 +0100

    Added stuff to second page

--- app/src/main/java/com/example/quote_on_quote_app/Page2Question.java
+++ app/src/main/java/com/example/quote_on_quote_app/Page2Question.java
@@ -2,7 +2,10 @@ package com.example.quote_on_quote_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Page2Question extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.quote-on-quote_app.MESSAGE";
@@ -10,5 +13,19 @@ public class Page2Question extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2_question);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Page3Question.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
+
