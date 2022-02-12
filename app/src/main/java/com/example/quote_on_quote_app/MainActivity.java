e9b1a753a50d1758213de665adfe476f185c4ee5
SophBr <sophie.breunig@yahoo.de>
2022-02-12 21:04:54 +0100

    Made everything look the same and adjusted margins

--- app/src/main/java/com/example/quote_on_quote_app/MainActivity.java
+++ app/src/main/java/com/example/quote_on_quote_app/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 
 /*
     public class MainActivity extends AppCompatActivity {
@@ -29,9 +31,6 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, StartQuestionnaire.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/quote_on_quote_app/QuoteOutput.java
+++ app/src/main/java/com/example/quote_on_quote_app/QuoteOutput.java
@@ -8,14 +8,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class QuoteOutput extends AppCompatActivity {
-    public static int EMOTION;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_output);
+
+        int emotion_1 = StartQuestionnaire.EMOTION;
+        int emotion_2 = Page2Question.EMOTION;
+
+        String label = String.format("Emotion 1: %d \n Emotion 2: %d", emotion_1, emotion_2);
+
+        TextView textView = (TextView) findViewById(R.id.textView9);
+        textView.setText(label);
     }
 
 }
