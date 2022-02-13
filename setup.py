2d317028a7a88196412fa1139282faaa5109a647
SophBr <sophie.breunig@yahoo.de>
2022-02-13 13:53:10 +0100

    tried to implement chaquopy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='quote-on-quote-app',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='sophiebreunig',
+    author_email='sophie.breunigyahoo.de',
+    description=''
+)
